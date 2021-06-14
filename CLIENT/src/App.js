93abf0642babc48dd86699684d7178c97eda1dc4
tae beok <joo98e@gmail.com>
2021-06-14 23:35:24 +0900

    good nas good

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Template from './components/template';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Template />
       </div>
     )
   }
