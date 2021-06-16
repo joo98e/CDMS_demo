3143e8791c9dc9a07d3824aeb46c334180f0cc7e
tae beok <joo98e@gmail.com>
2021-06-16 23:58:47 +0900

    0616 bcrypt please

--- SERVER/db/index.js
+++ SERVER/db/index.js
@@ -1,15 +1,28 @@
+const multer = require("multer");
 // ────────────────────────────────────────────────────────── Require
 const CURRENT_DIR = __dirname;
-const multer = require("multer");
-const connection = require("./db_connection");
 const app = require('./app');
 const port = process.env.PORT || 5005;
 const users = require('./routes/users');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const isDev = CURRENT_DIR.indexOf("C:") !== -1 ? `local is ${true}` : `local is ${false}`;
 
 app.use('/users', users);
 
 // ────────────────────────────────────────────────────────── 
 
-const isDev = CURRENT_DIR.indexOf("C:") !== -1 ? 'localhost' : 'service';
+const password = "12345";
+bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(`hash is ${hash}`);
+    });
+});
+setTimeout(() => {
+    const newPass = "12345";
+    console.log(`compareSync is ${bcrypt.compareSync(password, newPass)}`)    
+}, 3000);
+
+// ────────────────────────────────────────────────────────── 
 
 app.listen(port, () => console.log(`${isDev}, Listening on PORT ${port}`));
--- SERVER/db/routes/Register.js
+++ SERVER/db/routes/Register.js
@@ -8,9 +8,9 @@ router.get('/register', (req, res) => {
     // 회원가입시 필요한 정보들을 client에서 가져오면, 
     // 그것들을 DB에 넣는다.
 
-    const MEMBER_INFO = new User(req.body);
+    const MEMBER_INFO = req.body;
 
-
+    
 
 });
 
