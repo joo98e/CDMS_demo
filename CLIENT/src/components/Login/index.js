7b8bc62e5a7c6ede9ab6dd8f924ee6dbf2bb86e7
tae beok <joo98e@gmail.com>
2021-06-21 22:15:49 +0900

    register

--- CLIENT/src/components/Login/index.js
+++ CLIENT/src/components/Login/index.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { Box, Button } from '@material-ui/core'
+import { Container, Paper, Grid, Button, TextField } from '@material-ui/core'
 
 export class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user_id: '',
+            user_password: ''
+        }
+    }
+
     render() {
         return (
-            <Box display='flex' justifyContent="center" alignItems="space-around" pt={30}>
-                <Box>
-                    <Button>
-                        텍스트필드만들기
-                    </Button>
-                </Box>
-                <Box>
-                    <Button>
-                        sdf
-                    </Button>
-                </Box>
-            </Box>
+            <Container width={1280}>
+                <Paper>
+                    <Grid container justify='center'>
+                        <Grid item xs={12}>
+                            <Container>
+                                <TextField
+                                    fullWidth
+                                    variant="filled"
+                                >
+                                    1
+                                </TextField>
+                            </Container>
+                        </Grid>
+                        <Grid item xs={12} sm={1}>
+                            <Container>
+                                <Button
+                                    fullWidth
+                                >
+                                    1
+                                </Button>
+                            </Container>
+                        </Grid>
+                        <Grid item xs={12} sm={1}>
+                            <Container>
+                                <Button
+                                    fullWidth
+                                >
+                                    2
+                                </Button>
+                            </Container>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Container>
         )
     }
 }
