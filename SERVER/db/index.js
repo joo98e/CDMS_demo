605ddc78c855ff2d931fdddc81752a0b5b5e0f5f
joo98e <66362284+joo98e@users.noreply.github.com>
2021-07-17 17:37:20 +0900

    Proxy 관련 일부 커밋

--- SERVER/db/index.js
+++ SERVER/db/index.js
@@ -5,11 +5,13 @@ const port = process.env.PORT || 5005;
 const users = require('./routes/users');
 const departments = require('./routes/departments');
 const register = require('./routes/register');
+const menu = require('./routes/menu');
 const isDev = CURRENT_DIR.indexOf("C:") !== -1 ? `Dev is ${true}` : `Dev is ${false}`;
 
 app.use('/users', users);
 app.use('/departments', departments);
 app.use('/register', register);
+app.use('/menu', menu);
 
 // ────────────────────────────────────────────────────────── 
 
