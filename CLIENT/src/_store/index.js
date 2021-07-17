d9e73df105135a60a7981760b6ed4f61dbfd4b71
joo98e <66362284+joo98e@users.noreply.github.com>
2021-07-17 17:08:29 +0900

    Revert "123"
    This reverts commit b514384d2a5b2570e1181110def5a76416715ee5.

--- CLIENT/src/_store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import reducers from '../reducers';
-
-const store = createStore(reducers);
-store.subscribe(() => console.log(store.getState()));
-
-export default store;
