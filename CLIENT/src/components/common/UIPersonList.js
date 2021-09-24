6e6d134ca124267dce1d93e659874641ec93c47b
joo98e <66362284+joo98e@users.noreply.github.com>
2021-09-24 16:05:09 +0900

    0924_젠킨스 테스트 #5

--- CLIENT/src/components/common/UIPersonList.js
+++ CLIENT/src/components/common/UIPersonList.js
@@ -45,6 +45,8 @@ export class UIPersonList extends Component {
 
     componentDidMount() {
 
+        // 젠킨스 테스트 #5
+        console.log(21312321);
     }
 
     UIPersonRowChangeData = newItem => {
