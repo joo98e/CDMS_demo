ddc7e20247fd11b1d36d606aeb907d1945431b70
joo98e <66362284+joo98e@users.noreply.github.com>
2021-10-19 07:39:39 +0900

    1018_gitCache

--- SERVER/db/db_config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (() => {
-    return {
-        "host": "106.248.61.60",
-        "port": "1213",
-        "user": "myboard",
-        "password": "EVERYbeok1!2#",
-        "database": "CDMS"
-    }
-})(); 
