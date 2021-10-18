b50d774f8febe0ea87f655ef61601adb5e8bb463
joo98e <66362284+joo98e@users.noreply.github.com>
2021-10-18 22:26:08 +0900

    1018_프로세스추가수정중/쿼리짜야함

--- SERVER/db/db_connection.js
+++ SERVER/db/db_connection.js
@@ -3,15 +3,15 @@ const mysql = require('mysql');
 module.exports = (() => {
     const data = require('./db_config');
     const connection = mysql.createConnection({
-        host : data.host,
-        port : data.port,
-        user : data.user,
+        host     : data.host,
+        port     : data.port,
+        user     : data.user,
         password : data.password,
-        database : data.database,
+        database : data.database
     });
     
     connection.connect((err) => {
-        if(err) console.log(`connection err, ${err}`);
+        if(err) console.log(`MySQL 커넥션 에러, ${err}`);
         else{
             console.log(`${data.database} is Connected`);
         }
