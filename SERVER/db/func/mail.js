dc0bc588ddd12d857edd1157284c767e014b7380
askjmyyyojqa <65770016+askjmyyyojqa@users.noreply.github.com>
2021-10-27 17:12:59 +0900

    211027_mail.js json형태 매개변수 수정
    211027_mail.js json형태 매개변수 수정

--- SERVER/db/func/mail.js
+++ SERVER/db/func/mail.js
@@ -1,20 +1,20 @@
-function fnMail(_Tx, _Rx, _Subject, _Content) {
-    console.log(_Tx);
-    console.log(_Rx);
-    console.log(_Subject);
-    console.log(_Content);
+function fnMail(_json) {
+    console.log(_json);
+    // mail -s"$(echo -e '제목정도\nContent-type: text/html')" -r"icemira@mirimmedialab.co.kr" "askjmyyyojqa@gmail.com"<<<"<a href = 'www.naver.com'>내용정도</a>
     // 여기까지 값 오는 것 확인됨
-
     const exec = require('child_process').exec;
-    const strShell = "$(echo -e '" + _Subject + "\nContent-type: text/html')";
-    exec('mail -s"' + strShell + '" -r"' + _Tx + '" "' + _Rx + '"<<<"' + _Content + '"', {windowsHide : true}, function(err, stdout, stderr){
-        if(err){
-            console.log('메일 발송 중 오류가 발생했습니다.')
-        }
-        if(stdout){
-            console.log('메일 발송이 완료되었습니다.')
-        }
-    });
+    const strTransmit = 'mirimCDMS@cdms.mirimmedialab.co.kr';
+    for(i = 0; i < _json.length; i++){
+        const strShell = '$(echo -e "' + _json[i].subject + '\nContent-type: text/html")';
+        exec('mail -s"' + strShell + '" -r"' + strTransmit + '" "' + _json[i].receiver + '"<<<"' + _json[i].content + '"', {windowsHide : true}, function(err, stdout, stderr){
+            if(err){
+                console.log(i + '번째 메일 발송 중 오류가 발생했습니다.')
+            }
+            if(stdout){
+                console.log(i + '번째 메일 발송이 완료되었습니다.')
+            }
+        });
+    }
 }
 
 module.exports = fnMail;
