fc3346f75547b55e512d6c26d6b7b43a1d2f3064
askjmyyyojqa <65770016+askjmyyyojqa@users.noreply.github.com>
2021-11-11 14:38:05 +0900

    211111_테스트
    211111_테스트

--- SERVER/db/func/mail.js
+++ SERVER/db/func/mail.js
@@ -27,44 +27,6 @@ function fnMail() {
             sender : "유선재",				                    // 알림메일을 보내는 사람.
             content : "안녕하세요. 저는 ~~ 입니다...",	         //	메일을 보내는 사람이 적은 본문추가내용. 공백이면 기본내용만 전달.
         },
-        {
-            transmitter : "mirimCDMS",                          //	보내는 사람 메일주소. 공백이면 기본값 [mirimCDMS]로 변환
-            name : "알림메일",                                   //	보내는사람 메일주소 앞대가리에 붙는 이름
-            receiver : "askjmyyyojqa@gmail.com",                      //	받는사람 메일주소
-            reference : [				                        //	참조 목록. 공백이면 보내지 않음.
-
-            ],
-            secret : [					                        //	숨은 참조 목록. 공백이면 기본값 [strSecretDefault]로만 보냄.
-                // "askjmyyyojqa@gmail.com"	
-            ],
-            attach : [					                        //	첨부파일 경로. 공백이면 첨부하지 않음.
-                // "/raid/cdms/avatars/items/default/01.jpg",
-                // "/raid/cdms/avatars/items/default/02.jpg",
-            ],
-            subject : "[CDMS] 알림메일 입니다.",		         //	메일 제목
-            url : "www.naver.com",	                            //	게시판 url주소.
-            sender : "유선재",				                    // 알림메일을 보내는 사람.
-            content : "안녕하세요. 저는 ~~ 입니다...",	         //	메일을 보내는 사람이 적은 본문추가내용. 공백이면 기본내용만 전달.
-        },
-        {
-            transmitter : "mirimCDMS",                          //	보내는 사람 메일주소. 공백이면 기본값 [mirimCDMS]로 변환
-            name : "알림메일",                                   //	보내는사람 메일주소 앞대가리에 붙는 이름
-            receiver : "askjmyyyojqa@naver.com",                      //	받는사람 메일주소
-            reference : [				                        //	참조 목록. 공백이면 보내지 않음.
-
-            ],
-            secret : [					                        //	숨은 참조 목록. 공백이면 기본값 [strSecretDefault]로만 보냄.
-                // "askjmyyyojqa@gmail.com"	
-            ],
-            attach : [					                        //	첨부파일 경로. 공백이면 첨부하지 않음.
-                // "/raid/cdms/avatars/items/default/01.jpg",
-                // "/raid/cdms/avatars/items/default/02.jpg",
-            ],
-            subject : "[CDMS] 알림메일 입니다.",		         //	메일 제목
-            url : "www.naver.com",	                            //	게시판 url주소.
-            sender : "유선재",				                    // 알림메일을 보내는 사람.
-            content : "안녕하세요. 저는 ~~ 입니다...",	         //	메일을 보내는 사람이 적은 본문추가내용. 공백이면 기본내용만 전달.
-        }
     ]
 
     let _json = arrMailInfomation;
@@ -73,21 +35,32 @@ function fnMail() {
     const strTransmitterDefault = 'mirimCDMS';
     const strDomain = 'cdms.mirimmedialab.co.kr';
     const strSecretDefault = 'askjmyyyojqa@gmail.com';
+    const strStyle =              "<style>"
+                                +   "#warp {"
+                                +       "width : 100%;"
+                                +       "height : 100%;"
+                                +       "border : solid 1px black;"
+                                +       "font-size : 22px;"
+                                +   "}"
+                                +   "#warp > .content {"
+                                +       "display : block;"
+                                +   "}"
+                                + "</stlye>";
     let numCheckCount = 1;
     for(i = 0; i < _json.length; i++){
         const numLength = _json.length;
         const numCount = (i + 1);
-        const strTopText =        "--border\nContent-Type: text/html\n<div>"
-                                + "<span>안녕하세요. <b>미림미디어랩 CDMS</b> 입니다.</span>"
-                                + "<span>" + _json[i].sender + "님이 새로운 게시판을 작성하였습니다.</span>"
-                                + "<span>지금 바로 확인하시려면 <a href = '" + _json[i].url + "'>여기</a>를 눌러주세요.</span>";
+        const strTopText =        "<div id = 'wrap'>"
+                                +   "<span class = 'content'>안녕하세요. <b>미림미디어랩 CDMS</b> 입니다.</span>"
+                                +   "<span class = 'content'>" + _json[i].sender + "님이 새로운 게시판을 작성하였습니다.</span>"
+                                +   "<span class = 'content'>지금 바로 확인하시려면 <a href = '" + _json[i].url + "'>여기</a>를 눌러주세요.</span>";
 
         const strMiddleText = _json[i].content == undefined || _json[i].content == "" ? "" : "<span>다음은 " + _json[i].sender + "님이 보내는 메세지 입니다.</span><span>" + _json[i].content + "</span>";
 
-        const strBottomText =     "<span>확인 부탁드리겠습니다.</span>"
-                                + "<span>오늘도 좋은 하루 되세요.</span>"
-                                + "<span>감사합니다.</span>"
-                                + "</div>\n--border--";
+        const strBottomText =       "<span class = 'content'>확인 부탁드리겠습니다.</span>"
+                                +   "<span class = 'content'>오늘도 좋은 하루 되세요.</span>"
+                                +   "<span class = 'content'>감사합니다.</span>"
+                                + "</div>";
         const strTransmitter = _json[i].transmitter == undefined || _json[i].transmitter == '' ? strTransmitterDefault : _json[i].transmitter;
         const strName = _json[i].name == undefined || _json[i].name == '' ? "알림메일" : _json[i].name;
         const strReceiver = _json[i].receiver;
@@ -113,7 +86,7 @@ function fnMail() {
                 strAttach += '-a"' + _json[i].attach[c] + '" ';
             }
         }
-        exec('mail -s"' + strSubject + '" ' + strReference + strSecret + strAttach + '-r"' + strName + '<' + strTransmitter + '@' + strDomain + '>" "' + strReceiver + '"<<<"' + strTopText + strMiddleText + strBottomText + '"', {windowsHide : true}, function(err, stdout, stderr){
+        exec('mail -s"' + strSubject + '" ' + strReference + strSecret + strAttach + '-r"' + strName + '<' + strTransmitter + '@' + strDomain + '>" "' + strReceiver + '"<<<"' + strStyle + strTopText + strMiddleText + strBottomText + '"', {windowsHide : true}, function(err, stdout, stderr){
             if(err){
                 console.log(numCheckCount == 1 ? '*****************************************************************************************************\n총 ' + numLength + '개 중 ' + numCount + '번째 메일 발송에서 오류가 발생했습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver : numCheckCount == numLength ? '총 ' + numLength + '개 중 ' + numCount + '번째 메일 발송에서 오류가 발생했습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver + '\n*****************************************************************************************************' : '총 ' + numLength + '개 중 ' + numCount + '번째 메일 발송에서 오류가 발생했습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver);
             }
