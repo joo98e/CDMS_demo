539c5b1ceef7b35e36bbc2f1008897293ae5b2b5
askjmyyyojqa <65770016+askjmyyyojqa@users.noreply.github.com>
2021-11-11 09:03:21 +0900

    211111_테스트
    211111_테스트

--- SERVER/db/func/mail.js
+++ SERVER/db/func/mail.js
@@ -22,12 +22,14 @@ function fnMail() {
             receiver : "jtbeok@gmail.com",                      //	받는사람 메일주소
             reference : [				                        //	참조 목록. 공백이면 보내지 않음.
                 "qvud00@gmail.com",
+                "askjmyyyojqa@naver,com",
+                "askjmyyyojqa@daum.net"
             ],
             secret : [					                        //	숨은 참조 목록. 공백이면 기본값 [strSecretDefault]로만 보냄.
                 // "askjmyyyojqa@gmail.com"	
             ],
             attach : [					                        //	첨부파일 경로. 공백이면 첨부하지 않음.
-                // "/raid/cdms/avatars/items/default/01.jpg",
+                "/raid/cdms/avatars/items/default/01.jpg",
                 // "/raid/cdms/avatars/items/default/02.jpg",
             ],
             subject : "[CDMS] 알림메일 입니다.",		         //	메일 제목
@@ -44,8 +46,7 @@ function fnMail() {
     const strDomain = 'cdms.mirimmedialab.co.kr';
     const strSecretDefault = 'askjmyyyojqa@gmail.com';
     for(i = 0; i < _json.length; i++){
-        const strTopText =        "--frontier\n"
-                                + "<div>"
+        const strTopText =        "<div>"
                                 + "<span>안녕하세요. <b>미림미디어랩 CDMS</b> 입니다.</span>"
                                 + "<span>" + _json[i].sender + "님이 새로운 게시판을 작성하였습니다.</span>"
                                 + "<span>지금 바로 확인하시려면 <a href = '" + _json[i].url + "'>여기</a>를 눌러주세요.</span>";
@@ -55,9 +56,7 @@ function fnMail() {
         const strBottomText =     "<span>확인 부탁드리겠습니다.</span>"
                                 + "<span>오늘도 좋은 하루 되세요.</span>"
                                 + "<span>감사합니다.</span>"
-                                + "</div>"
-                                + "--frontier--";
-
+                                + "</div>";
         const strTransmitter = _json[i].transmitter == undefined || _json[i].transmitter == '' ? strTransmitterDefault : _json[i].transmitter;
         const strName = _json[i].name == undefined || _json[i].name == '' ? "알림메일" : _json[i].name;
         const strReceiver = _json[i].receiver;
