5c63aea3bf038b0e0518c601f02cdaa798434f16
askjmyyyojqa <65770016+askjmyyyojqa@users.noreply.github.com>
2021-11-11 10:55:57 +0900

    211111_테스트
    211111_테스트

--- SERVER/db/func/mail.js
+++ SERVER/db/func/mail.js
@@ -7,14 +7,6 @@ function fnMail() {
     // -c : 참조 목록
     // -b : 숨은 참조
     // -a : 참조 파일
-
-    // 메일 테스트 이력
-    // const strSubject = _json[i].subject + '$(echo -e \nContent-Type: text/html)' -> 실패. 결과 : [CDMS] 알림메일 입니다.-cqvud00@gmail.com
-    // \nContent-Type: text/plain -> 실패. 당연히 안됨.
-    // \nContent-Type: text/html -> 실패. 쉘스크립트로 바로 보내주거나 보낸사람에 이름을 지정하지 않고 보내는거면 성공함.
-    // \nMIME-Version: 1.0; -> 실패.
-    // \nMIME-Version: 1.0;Content-Type: multipart/alternative') -> 실패.
-    // \nMIME-Version: 1.0;Content-Type: multipart/related') -> 실패.
     let arrMailInfomation = [
         {
             transmitter : "mirimCDMS",                          //	보내는 사람 메일주소. 공백이면 기본값 [mirimCDMS]로 변환
@@ -82,6 +74,7 @@ function fnMail() {
     const strDomain = 'cdms.mirimmedialab.co.kr';
     const strSecretDefault = 'askjmyyyojqa@gmail.com';
     for(i = 0; i < _json.length; i++){
+        const numCount = (i + 1);
         const strTopText =        "<div>"
                                 + "<span>안녕하세요. <b>미림미디어랩 CDMS</b> 입니다.</span>"
                                 + "<span>" + _json[i].sender + "님이 새로운 게시판을 작성하였습니다.</span>"
@@ -99,7 +92,8 @@ function fnMail() {
         let strReference = '';
         let strSecret = '-b"' + strSecretDefault + '" ';
         let strAttach = '';
-        const strSubject = '$(echo -e "' + _json[i].subject + '\nContent-Type: text/html")';
+        const strSubject = '$(echo -e "' + _json[i].subject + '\nMIME-Version: 1.0; Content-Type: multipart/mixed; boundary=frontier")';
+        // const strSubject = '$(echo -e "' + _json[i].subject + '\nContent-Type: text/html")';
         const strSender = _json[i].sender;
 
         if(_json[i].reference != undefined && _json[i].reference.length != 0 && _json[i].reference != ""){
@@ -119,10 +113,10 @@ function fnMail() {
         }
         exec('mail -s"' + strSubject + '" ' + strReference + strSecret + strAttach + '-r"' + strName + '<' + strTransmitter + '@' + strDomain + '>" "' + strReceiver + '"<<<"' + strTopText + strMiddleText + strBottomText + '"', {windowsHide : true}, function(err, stdout, stderr){
             if(err){
-                console.log(i + '번째 메일 발송 중 오류가 발생했습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver);
+                console.log(numCount + '번째 메일 발송 중 오류가 발생했습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver);
             }
             if(stdout){
-                console.log(i + '번째 메일 발송이 완료되었습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver);
+                console.log(numCount + '번째 메일 발송이 완료되었습니다. 보내는 사람 : ' + strSender + ', 받는 사람 : ' + strReceiver);
             }
         });
     }
